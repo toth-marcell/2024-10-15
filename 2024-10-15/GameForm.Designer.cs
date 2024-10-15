bf6b2271cbea754e640012dcf1379bb257433652
toth-marcell <toth.marcell.21i@egressy.info>
2024-10-15 12:32:36 +0200

    game form labels

--- 2024-10-15/GameForm.Designer.cs
+++ 2024-10-15/GameForm.Designer.cs
@@ -29,13 +29,46 @@ namespace _2024_10_15
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "GameForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- 2024-10-15/GameForm.cs
+++ 2024-10-15/GameForm.cs
@@ -13,6 +13,8 @@ namespace _2024_10_15
             {
                 Environment.Exit(0);
             }
+            label1.Text = $"Hello, {Database.ActiveUser.Username}!";
+            label2.Text = $"Points: {Database.ActiveUser.Points}";
         }
     }
 }
